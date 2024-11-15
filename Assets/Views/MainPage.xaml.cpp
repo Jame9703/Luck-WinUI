87cb1181a081d927516251f306f51863901c09f4
zheyu-U <zheyu-U@users.noreply.github.com>
2024-11-15 23:09:33 +0800

    添加结果

--- Assets/Views/MainPage.xaml.cpp
+++ Assets/Views/MainPage.xaml.cpp
@@ -22,100 +22,89 @@ using namespace Windows::UI::Core;
 
 namespace winrt::Luck::implementation
 {
-    int32_t MainPage::MyProperty()
-    {
-        throw hresult_not_implemented();
-    }
+	int32_t MainPage::MyProperty()
+	{
+		throw hresult_not_implemented();
+	}
 
-    void MainPage::MyProperty(int32_t /* value */)
-    {
-        throw hresult_not_implemented();
-    }
+	void MainPage::MyProperty(int32_t /* value */)
+	{
+		throw hresult_not_implemented();
+	}
 
-    // 导航，获取窗口并储存在 this_AppWindow 中。
-    void MainPage::OnNavigatedTo(winrt::Microsoft::UI::Xaml::Navigation::NavigationEventArgs e)
-    {
-        using namespace winrt::Microsoft::UI::Xaml;
-       
-        try
-        {
-            Window w_window = e.Parameter().try_as<Window>();
-            if (!w_window) __debugbreak();
-            this_AppWindow = w_window.AppWindow();
-        }
-        catch (winrt::hresult_error const& ex)
-        {
-            winrt::hresult hr = ex.code(); 
-            winrt::hstring message = ex.message(); 
+	// 导航，获取窗口并储存在 this_AppWindow 中。
+	void MainPage::OnNavigatedTo(winrt::Microsoft::UI::Xaml::Navigation::NavigationEventArgs e)
+	{
+		using namespace winrt::Microsoft::UI::Xaml;
 
-            __debugbreak();
-            MessageBox(NULL, L"严重错误，窗口处理失败。\n", L"Luck错误", MB_OK);
-            abort();
-        }
+		try
+		{
+			Window w_window = e.Parameter().try_as<Window>();
+			if (!w_window) __debugbreak();
+			this_AppWindow = w_window.AppWindow();
+		}
+		catch (winrt::hresult_error const& ex)
+		{
+			winrt::hresult hr = ex.code();
+			winrt::hstring message = ex.message();
 
-        
-    }
+			__debugbreak();
+			MessageBox(NULL, L"严重错误，窗口处理失败。\n", L"Luck错误", MB_OK);
+			abort();
+		}
 
 
+	}
 
-    void MainPage::myButton_Click(IInspectable const&, RoutedEventArgs const&)
-    {
-        //myButton().Content(box_value(L"Clicked"));
-    }
-    void MainPage::Button_Click(IInspectable const& sender, RoutedEventArgs const& args)
-    {
-        LuckSet::SelectedNumber = 1;
-        MessageDialog dialog(L"抽奖进行中...");
-        switch (LuckSet::JudgePrize())
-        {
-        case 0:
-        {
-            dialog.Content(L"请选择你的幸运数字");
-            dialog.ShowAsync();
-        }
-        break;
-        case 1:
-        {
-            dialog.Content(L"恭喜你抽到一等奖");
-            dialog.ShowAsync();
-        }
-        break;
-        case 2:
-        {
-            dialog.Content(L"恭喜你抽到二等奖");
-            dialog.ShowAsync();
-        }
-        break;
-        case 3:
-        {
-            dialog.Content(L"恭喜你抽到三等奖");
-            dialog.ShowAsync();
-        }
-        break;
-        case -1:
-        {
-            dialog.Content(L"所有奖项均已抽完");
-            dialog.ShowAsync();
-        }
-        break;
-        }
-        MessageDialog dialog1(L"");
-        /*co_await*/ dialog1.ShowAsync();
-    }
-    void MainPage::ExitButton_Click(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args)
-    {
 
-        Application::Current().Exit();
-    }
-    void MainPage::RenewButton_Click(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args)
-    {
-        using namespace Microsoft::UI::Windowing;
-        if (this_AppWindow.Presenter().Kind() == AppWindowPresenterKind::FullScreen)
-            this_AppWindow.SetPresenter(AppWindowPresenterKind::Default);
-        else this_AppWindow.SetPresenter(AppWindowPresenterKind::FullScreen);
-    }
-    void MainPage::SettingsButton_Click(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args)
-    {
 
-    }
+	void MainPage::myButton_Click(IInspectable const&, RoutedEventArgs const&)
+	{
+		//myButton().Content(box_value(L"Clicked"));
+	}
+	void MainPage::Button_Click(IInspectable const& sender, RoutedEventArgs const& args)
+	{
+		using namespace Microsoft::UI::Xaml::Controls;
+		LuckSet::SelectedNumber = 1;
+
+
+		TextBlock name, dialogContent;
+		name.Text(L"结果");
+
+		ContentDialog dialog;
+		dialog.XamlRoot(this->XamlRoot());
+		dialog.Title(name);
+		dialog.DefaultButton(ContentDialogButton::Primary);
+
+
+		switch (LuckSet::JudgePrize())
+		{
+		case 0: { dialogContent.Text(L"请选择你的幸运数字"); break; }
+		case 1: { dialogContent.Text(L"恭喜你抽到一等奖"); break; }
+		case 2: { dialogContent.Text(L"恭喜你抽到二等奖"); break; }
+		case 3: { dialogContent.Text(L"恭喜你抽到三等奖"); break; }
+		case -1: { dialogContent.Text(L"所有奖项均已抽完"); break; }
+		}
+		dialog.Content(dialogContent);
+		dialog.PrimaryButtonText(L"好的");
+		dialog.SecondaryButtonText(L"不好");
+		dialog.ShowAsync();
+
+	}
+	void MainPage::ExitButton_Click(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args)
+	{
+
+		Application::Current().Exit();
+	}
+	void MainPage::RenewButton_Click(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args)
+	{
+		using namespace Microsoft::UI::Windowing;
+		if (this_AppWindow.Presenter().Kind() == AppWindowPresenterKind::FullScreen)
+			this_AppWindow.SetPresenter(AppWindowPresenterKind::Default);
+		else this_AppWindow.SetPresenter(AppWindowPresenterKind::FullScreen);
+	}
+	void MainPage::SettingsButton_Click(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args)
+	{
+
+	}
 }
