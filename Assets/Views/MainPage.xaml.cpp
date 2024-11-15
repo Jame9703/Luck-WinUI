aa4278013e5d8c886fc1f51a5948c1e2a31e4f26
Jame9703 <ncsb1213444@outlook.com>
2024-11-15 21:36:36 +0800

    #

--- Assets/Views/MainPage.xaml.cpp
+++ Assets/Views/MainPage.xaml.cpp
@@ -7,12 +7,15 @@
 #include "Assets\Classes\Luck-Class\Luck.h"
 #include <winrt/Windows.UI.Popups.h>
 #include <microsoft.ui.xaml.window.h>
+#include <windows.h>
+#include <winrt/Windows.UI.Core.h>
 
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
 using namespace Windows::UI::Popups;
 using namespace winrt::Microsoft::UI;
 using namespace winrt::Microsoft::UI::Windowing;
+using namespace Windows::UI::Core;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -84,13 +87,55 @@ namespace winrt::Luck::implementation
         //Get access to IWindowNativewinrt::com_ptr<::IWindowNative>windowNative= warningWindow_create.as<IWindowNative>()
         // Get the HWND for the XAML Window
                 //Get access to IWindowNative
-        auto window = Window::Current();
-        winrt::com_ptr<::IWindowNative> windowNative = window as<IWindowNative>();
-        HWND hWnd;
-        windowNative->get_WindowHandle(&hWnd);
-        WindowId id = GetwindowIdFromwindow(hWnd);
-        winrt::Microsoft::UI::Windowing::AppWindow appwind = winrt::Microsoft::UI::Windowing::AppWindow::GetFromWindowId(id);
-        appwind.SetPresenter(AppWindowPresenterKind::FullScreen);
+        //auto window = Window::Current();
+        //winrt::com_ptr<::IWindowNative> windowNative = window as<IWindowNative>();
+        //HWND hWnd;
+        //windowNative->get_WindowHandle(&hWnd);
+        //WindowId id = GetwindowIdFromwindow(hWnd);
+        //winrt::Microsoft::UI::Windowing::AppWindow appwind = winrt::Microsoft::UI::Windowing::AppWindow::GetFromWindowId(id);
+        //appwind.SetPresenter(AppWindowPresenterKind::FullScreen);
+
+        // 像是一个全屏
+        //HWND hWnd = GetActiveWindow();
+        //if (hWnd != nullptr)
+        //{
+        //    // 获取屏幕尺寸
+        //    int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+        //    int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+        //    // 设置窗口位置和大小为全屏
+        //    SetWindowPos(hWnd, HWND_TOP, 0, 0, screenWidth, screenHeight, SWP_SHOWWINDOW);
+        //}
+
+        //样式很奇怪
+        HWND hWnd = GetActiveWindow();
+        if (hWnd != nullptr)
+        {
+            //// 获取屏幕尺寸
+            //int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+            //int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+            //// 隐藏窗口标题栏
+            //LONG_PTR style = GetWindowLongPtr(hWnd, GWL_STYLE);
+            //style &= ~WS_CAPTION;
+            //SetWindowLongPtr(hWnd, GWL_STYLE, style);
+
+            //// 设置窗口位置和大小为全屏
+            //SetWindowPos(hWnd, HWND_TOP, 0, 0, screenWidth, screenHeight, SWP_SHOWWINDOW);
+            AppWindow appWindow = AppWindow::GetFromWindow(hWnd);
+            if (appWindow)
+            {
+                // 获取屏幕尺寸
+                int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+                int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+                appWindow.Resize({ screenWidth, screenHeight });
+                appWindow.Move({ 0,0 });
+            }
+        }
+        // 保持应用运行，这里可以根据实际情况修改逻辑
+        // 例如，可以添加一个消息循环等
+        //CoreWindow::GetForCurrentThread().Dispatcher().ProcessEvents(CoreProcessEventsOption::ProcessUntilQuit);
     }
     void MainPage::SettingsButton_Click(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args)
     {
